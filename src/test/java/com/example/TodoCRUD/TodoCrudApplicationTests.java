3a64c71b5270b2d1701afcc7c8ae0b0c7f35fd9b
cosmosthewill <93437373+cosmosthewill@users.noreply.github.com>
2025-06-06 08:25:36 +0700

    first commit

---
+++ src/test/java/com/example/TodoCRUD/TodoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodoCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
