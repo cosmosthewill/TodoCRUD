5a974cefa2f04b163b46b1eebcdfeb702dd7042e
cosmosthewill <93437373+cosmosthewill@users.noreply.github.com>
2025-06-06 09:04:37 +0700

    add base crud
    (cherry picked from commit b81ddea67d0713a59f97768445bbae151390865e)

---
+++ src/main/java/com/example/TodoCRUD/controller/TaskController.java
@@ -0,0 +1,8 @@
+package com.example.TodoCRUD.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+}
--- src/test/java/com/example/TodoCRUD/TaskCrudApplicationTests.java
+++ src/test/java/com/example/TodoCRUD/TaskCrudApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodoCrudApplicationTests {
+class TaskCrudApplicationTests {
 
 	@Test
 	void contextLoads() {
