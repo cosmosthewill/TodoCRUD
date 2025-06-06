0d036ee3bfc0a2db390609fbd560e7387e693372
cosmosthewill <93437373+cosmosthewill@users.noreply.github.com>
2025-06-06 09:31:25 +0700

    create task

--- src/main/java/com/example/TodoCRUD/controller/TaskController.java
+++ src/main/java/com/example/TodoCRUD/controller/TaskController.java
@@ -1,8 +1,22 @@
 package com.example.TodoCRUD.controller;
 
+import com.example.TodoCRUD.model.Task;
+import com.example.TodoCRUD.service.CreateTaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+    private final CreateTaskService createTaskService;
+    public TaskController(CreateTaskService createTaskService) {
+        this.createTaskService = createTaskService;
+    }
 
+    @PostMapping("/add_task")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        return createTaskService.execute(task);
+    }
 }
