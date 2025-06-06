a33df9652c944a41b180dd4c7e9d311eb2a2c3b5
cosmosthewill <93437373+cosmosthewill@users.noreply.github.com>
2025-06-06 09:23:11 +0700

    add read all tasks

--- src/main/java/com/example/TodoCRUD/controller/TaskController.java
+++ src/main/java/com/example/TodoCRUD/controller/TaskController.java
@@ -1,8 +1,24 @@
 package com.example.TodoCRUD.controller;
 
+import com.example.TodoCRUD.model.Task;
+import com.example.TodoCRUD.service.GetTasksService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
 
+    private final GetTasksService getTasksService;
+
+    public TaskController(GetTasksService getTasksService) {
+        this.getTasksService = getTasksService;
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> getTasks() {
+        return getTasksService.execute(null);
+    }
 }
